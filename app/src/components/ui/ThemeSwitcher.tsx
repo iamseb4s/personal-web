e62e14293f667e4bbba9f75d3811065773cedc6d
Sebastian Nolasco <sebasnolascop@gmail.com>
2025-09-01 21:13:58 -0500

    feat: implement static header and footer components

---
+++ app/src/components/ui/ThemeSwitcher.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThemeSwitcher = () => {
+  return (
+    <button>
+      {/* Icon and logic will be added in a future task */}
+      Theme
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
