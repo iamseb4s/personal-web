21742e74657993adb824e2991b20bf33b46aaf43
Sebas N. <sebas@iamsebas.dev>
2025-11-11 03:50:43 -0500

    feat(web): Add Strapi data fetching library and dependencies
    - Creates a new `strapi.ts` library with helper functions (`fetchAPI`,
    `getStrapiURL`) to communicate with the Strapi API.
    - Adds 'qs' and 'reading-time' as dependencies to support API calls and
    content processing.

---
+++ apps/web/src/lib/strapi.ts
@@ -0,0 +1,70 @@
+import qs from 'qs';
+
+/**
+ * Get full Strapi URL from path
+ * @param {string} path Path of the URL
+ * @returns {string} Full Strapi URL
+ */
+export function getStrapiURL(path = '') {
+  const strapiUrl =
+    typeof window === 'undefined'
+      ? process.env.STRAPI_INTERNAL_URL
+      : process.env.NEXT_PUBLIC_STRAPI_API_URL;
+
+  return `${strapiUrl || 'http://localhost:1337'}${path}`;
+}
+
+/**
+ * Helper to make GET requests to Strapi API
+ * @param {string} path Path of the API route
+ * @param {object} urlParamsObject URL params object, will be stringified
+ * @param {object} options Options passed to fetch
+ * @returns {Promise} Parsed API call response
+ */
+export async function fetchAPI(
+  path: string,
+  urlParamsObject = {},
+  options = {}
+) {
+  // Merge default and user options
+  const mergedOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+    ...options,
+  };
+
+  // Build request URL
+  const queryString = qs.stringify(urlParamsObject);
+  const requestUrl = `${getStrapiURL(
+    `/api${path}${queryString ? `?${queryString}` : ''}`
+  )}`;
+
+  // Trigger API call
+  const response = await fetch(requestUrl, mergedOptions);
+
+  // Handle response
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error(`An error occurred please try again`);
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function getProjectBySlugFromAPI(slug: string) {
+  const data = await fetchAPI('/projects', {
+    filters: {
+      slug: {
+        $eq: slug,
+      },
+    },
+    populate: '*', // Populate all fields
+  });
+  // The response for a filtered query is an array. We need to return the first element.
+  if (data && data.data && data.data.length > 0) {
+    return data.data[0];
+  }
+  return null;
+}
